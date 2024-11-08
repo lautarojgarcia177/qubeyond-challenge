66f46b1e9d55dc7f2faccb334cddff993c5509fb
Lautaro Garcia <lautarojgarcia177@gmail.com>
2024-11-08 10:57:43 -0300

    cache invalidation

--- server/api/joke-like.patch.ts
+++ server/api/joke-like.patch.ts
@@ -1,22 +1,37 @@
 import { defineEventHandler } from "h3";
 import { Joke } from "~/interfaces";
 
+async function invalidateCache() {
+  //cache invalidation
+  const cacheStorage = useStorage("cache:nitro");
+  const cachedKeys = await cacheStorage.getKeys();
+  await Promise.all(cachedKeys.map((key) => cacheStorage.removeItem(key)));
+}
+
 export default defineEventHandler(async (event) => {
+  await invalidateCache();
+  
   const { id } = await readBody(event);
   try {
-    let jokes = await useStorage('assets:server').getItem('jokes.json') as Joke[];
+    let jokes = (await useStorage("assets:server").getItem(
+      "jokes.json"
+    )) as Joke[];
     const jokeToLike = jokes.find((joke) => joke.id === id);
     if (jokeToLike) {
       jokeToLike.likes++;
-      await useStorage('assets:server').setItemRaw('jokes.json', JSON.stringify(jokes, null, 2))
+      await useStorage("assets:server").setItemRaw(
+        "jokes.json",
+        JSON.stringify(jokes, null, 2)
+      );
     } else {
       throw createError({
         statusCode: 400,
         statusMessage: "The joke to like does not exist",
       });
     }
-    jokes = await useStorage('assets:server').getItem('jokes.json') as Joke[]
-    return jokes;
+    await invalidateCache();
+    jokes = (await useStorage("assets:server").getItem("jokes.json")) as Joke[];
+    return jokes.find((joke) => joke.id === id);
   } catch (error) {
     throw createError({
       statusMessage: "Failed to like the joke",
